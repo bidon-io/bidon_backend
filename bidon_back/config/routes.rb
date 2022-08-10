4eba0026c7a5f1a1cd94b6fb5ff4b7365839bee6
KI <ki@bidon.org>
2022-08-10 15:55:59 +0200

    Add avohq admin panel

--- bidon_back/config/routes.rb
+++ bidon_back/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Avo::Engine, at: Avo.configuration.root_path
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
