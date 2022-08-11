faa7b57d7bc71011e0fc7e66366ad39672286a38
KI <ki@bidon.org>
2022-08-11 23:04:47 +0200

    Add connection to Appodeal PG

--- bidon_back/config/puma.rb
+++ bidon_back/config/puma.rb
@@ -1,12 +1,11 @@
-workers Integer(ENV['PUMA_WORKER_COUNT'] || 2)
-threads_count = Integer(ENV['PUMA_THREADS_COUNT'] || 5)
+workers Integer(ENV['RAILS_PUMA_WORKER_COUNT'] || 2)
+threads_count = Integer(ENV['RAILS_THREADS_COUNT'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
 port ENV.fetch('PORT', 3000)
 environment ENV.fetch('RAILS_ENV', 'development')
-pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
