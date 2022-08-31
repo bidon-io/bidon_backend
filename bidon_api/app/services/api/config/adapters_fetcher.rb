ca647303301dc1deec34622b9a51aa2aaba79134
AJ <aj@bidon.org>
2022-08-31 18:27:41 +0200

    [BDN-13] Add applovin adapter

--- bidon_api/app/services/api/config/adapters_fetcher.rb
+++ bidon_api/app/services/api/config/adapters_fetcher.rb
@@ -21,35 +21,63 @@ module Api
       end
       memo_wise :app_mmp_profile
 
+      def applovin_demand_profile
+        AppDemandProfile.eager(:demand_source_account)
+                        .where(app_id: app.id, account_type: 'DemandSourceAccount::Applovin').first
+      end
+      memo_wise :applovin_demand_profile
+
       private
 
-      def fetch_adapter(adapter_name) # rubocop:disable Metrics/MethodLength
-        return {} unless app_mmp_profile
-
+      def fetch_adapter(adapter_name)
         case adapter_name.to_s
         when 'appsflyer'
-          {
-            dev_key: app_mmp_profile.appsflyer_dev_key,
-            app_id:  app_mmp_profile.appsflyer_app_id,
-          }
+          fetch_appsflyer_adapter
         when 'adjust'
-          {
-            app_token: app_mmp_profile.adjust_app_token,
-            s2s_token: app_mmp_profile.adjust_s2s_token,
-          }
+          fetch_adjust_adapter
         when 'bidmachine'
-          {
-            seller_id:        '1',
-            endpoint:         'x.appbaqend.com',
-            mediation_config: %w[meta_audience criteo pangle amazon adcolony my_target vungle tapjoy notsy],
-          }
+          fetch_bidmachine_adapter
         when 'applovin'
-          {
-            app_key: 'example',
-          }
+          fetch_applovin_adapter
         else
           {}
         end
+      end
+
+      def fetch_appsflyer_adapter
+        return {} unless app_mmp_profile
+
+        {
+          dev_key: app_mmp_profile.appsflyer_dev_key,
+          app_id:  app_mmp_profile.appsflyer_app_id,
+        }
+      end
+
+      def fetch_adjust_adapter
+        return {} unless app_mmp_profile
+
+        {
+          app_token: app_mmp_profile.adjust_app_token,
+          s2s_token: app_mmp_profile.adjust_s2s_token,
+        }
+      end
+
+      def fetch_bidmachine_adapter
+        {
+          seller_id:        '1',
+          endpoint:         'x.appbaqend.com',
+          mediation_config: %w[meta_audience criteo pangle amazon adcolony my_target vungle tapjoy notsy],
+        }
+      end
+
+      def fetch_applovin_adapter
+        return {} unless applovin_demand_profile
+
+        extra = JSON.parse(applovin_demand_profile.demand_source_account.extra)
+
+        {
+          app_key: extra['api_key'],
+        }
       end
     end
   end
