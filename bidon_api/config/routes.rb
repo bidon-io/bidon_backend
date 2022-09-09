77ffa49600d955080ea0157ce75819a71075628e
AJ <aj@bidon.org>
2022-09-09 14:38:24 +0200

    [BDN-17] Add new api urls (#50)

--- bidon_api/config/routes.rb
+++ bidon_api/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   post 'config', to: 'config#create'
   post 'auction/:ad_type', to: 'auction#create'
-
   post 'stats/:ad_type',   to: 'stats#create'
   post 'click/:ad_type',   to: 'click#create'
   post 'show/:ad_type',    to: 'show#create'
-  post 'reward/:ad_type',  to: 'reward#create'
+  post 'reward/rewarded',  to: 'reward#create'
+
+  post ':ad_type/auction', to: 'auction#create'
+  post ':ad_type/stats',   to: 'stats#create'
+  post ':ad_type/click',   to: 'click#create'
+  post ':ad_type/show',    to: 'show#create'
+  post 'rewarded/reward',  to: 'reward#create'
 end
