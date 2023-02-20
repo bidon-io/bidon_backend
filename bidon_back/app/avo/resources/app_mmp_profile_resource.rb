447a54a650058c4727b27d0fd7b7dd013d5d986e
AJ <aj@bidon.org>
2023-02-20 19:20:41 +0100

    [BDN-55] Update AppMmpProfileResource (#70)

--- bidon_back/app/avo/resources/app_mmp_profile_resource.rb
+++ bidon_back/app/avo/resources/app_mmp_profile_resource.rb
@@ -2,34 +2,20 @@ class AppMmpProfileResource < Avo::BaseResource
   self.title = :human_name
 
   field :id, as: :id
+  field :app, as: :belongs_to, required: true
   field :start_date, as: :date, required: true
-  field :mmp_platform,
-        as:       :select,
-        required: true,
-        options:  {
-          None:      0,
-          Adjust:    2,
-          Appsflyer: 1,
-        }
+  field :mmp_platform, as: :select, required: true, enum: AppMmpProfile.mmp_platforms
   field :mmp_account_primary, as: :belongs_to, required: true
   field :mmp_account_secondary, as: :belongs_to, required: true
   field :get_spend_from_secondary_mmp_account, as: :boolean
   field :primary_mmp_raw_data_source,
         as:       :select,
         required: true,
-        options:  {
-          None:      0,
-          Adjust:    2,
-          Appsflyer: 1,
-        }
+        enum:     AppMmpProfile.primary_mmp_raw_data_sources
   field :secondary_mmp_raw_data_source,
         as:       :select,
         required: true,
-        options:  {
-          None:      0,
-          Adjust:    2,
-          Appsflyer: 1,
-        }
+        enum:     AppMmpProfile.secondary_mmp_raw_data_sources
   field :adjust_app_token, as: :text
   field :appsflyer_dev_key, as: :text
   field :appsflyer_conversion_keys, as: :text
