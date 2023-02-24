195a1011f086eae4bc76f353d914d46a9b4e5d00
AJ <aj@bidon.org>
2023-02-24 12:50:13 +0100

    [BDN-XXX] Fix line_item ad_type in admin panel (#76)

--- bidon_back/app/avo/resources/mmp_account_resource.rb
+++ bidon_back/app/avo/resources/mmp_account_resource.rb
@@ -11,12 +11,12 @@ class MmpAccountResource < Avo::BaseResource
           Appsflyer: 'appsflyer',
         }
   field :is_global_account, as: :boolean, required: true
-  field :use_s3, as: :boolean
-  field :s3_access_key_id, as: :text
-  field :s3_secret_access_key, as: :text
-  field :s3_bucket_name, as: :text
-  field :s3_region, as: :text
-  field :s3_home_folder, as: :text
-  field :master_api_token, as: :text
-  field :user_token, as: :text
+  field :use_s3, as: :boolean, hide_on: :index
+  field :s3_access_key_id, as: :text, hide_on: :index
+  field :s3_secret_access_key, as: :text, hide_on: :index
+  field :s3_bucket_name, as: :text, hide_on: :index
+  field :s3_region, as: :text, hide_on: :index
+  field :s3_home_folder, as: :text, hide_on: :index
+  field :master_api_token, as: :text, hide_on: :index
+  field :user_token, as: :text, hide_on: :index
 end
