9e3ef97dd741f68d8657e439135f3143309f5da4
AJ <aj@bidon.org>
2023-03-09 11:14:07 +0100

    [BDN-XXX] Update package_name uniqueness (#98)

--- bidon_back/app/models/app.rb
+++ bidon_back/app/models/app.rb
@@ -5,8 +5,9 @@ class App < ApplicationRecord
   has_many :line_items,             dependent: :restrict_with_exception
   has_many :auction_configurations, dependent: :destroy
 
-  validates :package_name, :app_key,    presence: true, uniqueness: true
-  validates :platform_id,  :human_name, presence: true
+  validates :app_key, presence: true, uniqueness: true
+  validates :package_name, :platform_id, :human_name, presence: true
+  validates :package_name, uniqueness: { scope: :platform_id }
 
   enum platform_id: { ios: 4, android: 1 }
 
--- bidon_back/db/schema.rb
+++ bidon_back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_161058) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_100412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_161058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["app_key"], name: "index_apps_on_app_key", unique: true
-    t.index ["package_name"], name: "index_apps_on_package_name", unique: true
+    t.index ["package_name", "platform_id"], name: "index_apps_on_package_name_and_platform_id", unique: true
     t.index ["user_id"], name: "index_apps_on_user_id"
   end
 
