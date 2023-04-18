1a0961f62c3504e4351a391b028b996fe82a2378
ID <id@bidon.org>
2023-04-18 15:36:40 +0200

    [BDN-137] Fix auction rounds response (#119)
    * [BDN-137] Fix auction rounds response
    
    * [BDN-137] Update tests

--- bidon_api/spec/services/api/auction/rounds_filterer_spec.rb
+++ bidon_api/spec/services/api/auction/rounds_filterer_spec.rb
@@ -3,147 +3,94 @@
 require 'rails_helper'
 
 RSpec.describe Api::Auction::RoundsFilterer do
-  context 'adapter is present only in round 1' do
+  subject(:filterer) { described_class.new(rounds:, adapters:) }
+
+  let(:adapters) do
+    {
+      'unityads' => {
+        'version'     => '0.1.1.0-Beta',
+        'sdk_version' => '4.5.0',
+      },
+    }
+  end
+
+  context 'when round has networks that are not present in adapters' do
     let(:rounds) do
       [
         {
-          'id'      => 'ROUND_INTERSTITIAL_1',
-          'demands' => %w[admob bidmachine],
-          'timeout' => 15_000,
-        },
-        {
-          'id'      => 'ROUND_INTERSTITIAL_2',
-          'demands' => %w[bidmachine],
+          'id'      => 'ROUND_BANNER_2',
+          'demands' => %w[admob unityads applovin],
           'timeout' => 15_000,
         },
       ]
     end
 
-    let(:adapters) do
-      {
-        'admob' => {
-          'version'     => '0.1.0.1-Beta',
-          'sdk_version' => '21.5.0',
-        },
-      }
-    end
-
-    let(:expected_result) do
-      [
-        {
-          'id'      => 'ROUND_INTERSTITIAL_1',
-          'demands' => %w[admob],
-          'timeout' => 15_000,
-        },
-      ]
-    end
-
-    it 'returns filtered rounds in 1 round' do
-      expect(described_class.new(rounds:, adapters:).fetch).to eq expected_result
+    it 'filters not present networks from this round' do
+      expect(filterer.fetch).to eq(
+        [
+          {
+            'id'      => 'ROUND_BANNER_2',
+            'demands' => %w[unityads],
+            'timeout' => 15_000,
+          },
+        ],
+      )
     end
   end
 
-  context 'adapter is not present in rounds' do
+  context 'when round does not have any network from adapters' do
     let(:rounds) do
       [
         {
-          'id'      => 'ROUND_INTERSTITIAL_1',
-          'demands' => %w[bidmachine],
-          'timeout' => 15_000,
-        },
-        {
-          'id'      => 'ROUND_INTERSTITIAL_2',
-          'demands' => %w[bidmachine],
+          'id'      => 'ROUND_BANNER_3',
+          'demands' => %w[dtexchange bidmachine],
           'timeout' => 15_000,
         },
       ]
     end
 
-    let(:adapters) do
-      {
-        'admob' => {
-          'version'     => '0.1.0.1-Beta',
-          'sdk_version' => '21.5.0',
-        },
-      }
-    end
-
-    let(:expected_result) { [] }
-
-    it 'returns empty array' do
-      expect(described_class.new(rounds:, adapters:).fetch).to eq expected_result
+    it 'does not return this round' do
+      expect(filterer.fetch).to eq([])
     end
   end
 
-  context 'no adapters passed' do
+  context 'when configuration has multiple rounds' do
     let(:rounds) do
       [
         {
-          'id'      => 'ROUND_INTERSTITIAL_1',
+          'id'      => 'ROUND_BANNER_1',
           'demands' => %w[bidmachine],
           'timeout' => 15_000,
         },
         {
-          'id'      => 'ROUND_INTERSTITIAL_2',
-          'demands' => %w[bidmachine],
+          'id'      => 'ROUND_BANNER_2',
+          'demands' => %w[admob unityads applovin],
+          'timeout' => 15_000,
+        },
+        {
+          'id'      => 'ROUND_BANNER_3',
+          'demands' => %w[dtexchange bidmachine unityads],
           'timeout' => 15_000,
         },
       ]
     end
 
-    let(:adapters) do
-      {}
-    end
+    it 'filters each round independently' do
+      expect(filterer.fetch).to eq(
+        [
+          {
+            'id'      => 'ROUND_BANNER_2',
+            'demands' => %w[unityads],
+            'timeout' => 15_000,
+          },
+          {
+            'id'      => 'ROUND_BANNER_3',
+            'demands' => %w[unityads],
+            'timeout' => 15_000,
+          },
 
-    let(:expected_result) { [] }
-
-    it 'returns empty array' do
-      expect(described_class.new(rounds:, adapters:).fetch).to eq expected_result
-    end
-  end
-
-  context 'adapter is present only in rounds 1 and 3' do
-    let(:rounds) do
-      [
-        {
-          'id'      => 'ROUND_INTERSTITIAL_1',
-          'demands' => %w[admob bidmachine],
-          'timeout' => 15_000,
-        },
-        {
-          'id'      => 'ROUND_INTERSTITIAL_2',
-          'demands' => %w[bidmachine],
-          'timeout' => 15_000,
-        },
-        {
-          'id'      => 'ROUND_INTERSTITIAL_3',
-          'demands' => %w[admob bidmachine],
-          'timeout' => 15_000,
-        },
-      ]
-    end
-
-    let(:adapters) do
-      {
-        'admob' => {
-          'version'     => '0.1.0.1-Beta',
-          'sdk_version' => '21.5.0',
-        },
-      }
-    end
-
-    let(:expected_result) do
-      [
-        {
-          'id'      => 'ROUND_INTERSTITIAL_1',
-          'demands' => %w[admob],
-          'timeout' => 15_000,
-        },
-      ]
-    end
-
-    it 'returns filtered rounds in 1 round' do
-      expect(described_class.new(rounds:, adapters:).fetch).to eq expected_result
+        ],
+      )
     end
   end
 end
