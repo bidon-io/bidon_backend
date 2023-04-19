3041d71c897ad38a9f2345fcb1aee5fa92023990
ID <id@bidon.org>
2023-04-19 15:31:05 +0200

    [BDN-133] Add loss events consuming (#121)
    * [BDN-133] Add loss events consuming
    
    * [BDN-133] Add KAFKA_LOSS_TOPIC env variable

--- bidon_api/app/controllers/loss_controller.rb
+++ bidon_api/app/controllers/loss_controller.rb
@@ -2,7 +2,9 @@
 
 class LossController < ApplicationController
   def create
-    Rails.logger.info("LossController#create: #{permitted_params}")
+    kafka_event = KafkaEvent.new(params: permitted_params, ip: request.remote_ip).build
+    KafkaLogger.log_loss(kafka_event)
+
     render_empty_result
   end
 
