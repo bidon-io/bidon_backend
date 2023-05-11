113eaa057b164e73a3fc1551481aa9751aa3c9d6
AJ <aj@bidon.org>
2023-05-11 16:15:18 +0200

    [BDN-166] Log config events (#135)
    * [BDN-166] Log config and auction events
    
    * [BDN-166] Remove auction from loggin
    
    * [BDN-166] Add param to composer

--- bidon_api/app/controllers/loss_controller.rb
+++ bidon_api/app/controllers/loss_controller.rb
@@ -2,7 +2,6 @@
 
 class LossController < ApplicationController
   def create
-    kafka_event = KafkaEvent.new(params: permitted_params, ip: request.remote_ip).build
     KafkaLogger.log_loss(kafka_event)
 
     render_empty_result
