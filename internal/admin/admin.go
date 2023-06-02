3c1191c259277703c46f2b498ad376a496260d8f
ID <id@bidon.org>
2023-06-02 10:44:55 +0200

    [BDN-222/simplify-route-registering (#157)

--- internal/admin/admin.go
+++ internal/admin/admin.go
@@ -2,6 +2,8 @@
 package admin
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,21 +14,38 @@ type Service struct {
 }
 
 func (s *Service) RegisterAPIRoutes(g *echo.Group) {
-	g.GET("/auction_configurations", s.AuctionConfigurations.list)
-	g.POST("/auction_configurations", s.AuctionConfigurations.create)
-	g.GET("/auction_configurations/:id", s.AuctionConfigurations.get)
-	g.PATCH("/auction_configurations/:id", s.AuctionConfigurations.update)
-	g.DELETE("/auction_configurations/:id", s.AuctionConfigurations.delete)
+	resources := []resourceRoutes{
+		{"auction_configurations", s.AuctionConfigurations},
+		{"apps", s.Apps},
+		{"segments", s.Segments},
+	}
 
-	g.GET("/apps", s.Apps.list)
-	g.POST("/apps", s.Apps.create)
-	g.GET("/apps/:id", s.Apps.get)
-	g.PATCH("/apps/:id", s.Apps.update)
-	g.DELETE("/apps/:id", s.Apps.delete)
+	for i := range resources {
+		resource := &resources[i]
+		resource.register(g)
+	}
+}
 
-	g.GET("/segments", s.Segments.list)
-	g.POST("/segments", s.Segments.create)
-	g.GET("/segments/:id", s.Segments.get)
-	g.PATCH("/segments/:id", s.Segments.update)
-	g.DELETE("/segments/:id", s.Segments.delete)
+type resourceRoutes struct {
+	name    string
+	handler resourceHandler
+}
+
+func (rr *resourceRoutes) register(g *echo.Group) {
+	collectionPath := fmt.Sprintf("/%s", rr.name)
+	itemPath := fmt.Sprintf("%s/:id", collectionPath)
+
+	g.GET(collectionPath, rr.handler.list)
+	g.POST(collectionPath, rr.handler.create)
+	g.GET(itemPath, rr.handler.get)
+	g.PATCH(itemPath, rr.handler.update)
+	g.DELETE(itemPath, rr.handler.delete)
+}
+
+type resourceHandler interface {
+	list(echo.Context) error
+	create(echo.Context) error
+	get(echo.Context) error
+	update(echo.Context) error
+	delete(echo.Context) error
 }
