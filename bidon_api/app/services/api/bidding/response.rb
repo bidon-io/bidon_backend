da8eb76abeb87f7566ba12fa2791e7afb2bdc82f
KI <ki@bidon.org>
2023-06-02 10:42:55 +0200

    [BDN-223] Add minimal bidding endpoint (#156)

---
+++ bidon_api/app/services/api/bidding/response.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Api
+  module Bidding
+    DemandResponse = Struct.new(:demand, :raw_request, :raw_response, :status, :price, :seatbid, keyword_init: true)
+
+    class Response
+      prepend MemoWise
+
+      attr_reader :bid_request
+
+      delegate :app, :ad_type, :device, to: :bid_request
+
+      def initialize(bid_request)
+        @bid_request = bid_request
+      end
+
+      def bid?
+        seat_bid[:price] > 0
+      end
+
+      def body
+        {
+          id:      SecureRandom.uuid,
+          seatbid: seat_bid[:seatbid],
+        }
+      end
+      memo_wise :body
+
+      private
+
+      def imp
+        bid_request.params['imp'][0]
+      end
+      memo_wise :imp
+
+      def seat_bid
+        demands = imp.dig(:ext, :bidon, :bidding)
+
+        responses = demands.map do |demand, hash|
+          request_demand(demand, hash[:token], imp[:bidfloor]).call
+        end
+
+        responses.max_by(&:price)
+      end
+      memo_wise :seat_bid
+
+      def request_demand(demand, token, bidfloor)
+        case demand.to_s
+        when 'bidmachine'
+          Bidding::Demand::BidMachine.new(bid_request, token, bidfloor)
+        else
+          -> { DemandResponse.new(price: 0) }
+        end
+      end
+    end
+  end
+end
