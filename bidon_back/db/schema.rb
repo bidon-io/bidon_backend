d29a5957bd4a436a9617f6eced7be391a23d4e7c
AJ <aj@bidon.org>
2023-06-13 16:21:11 +0200

    [BDN-193] Add priority to segment (#178)

--- bidon_back/db/schema.rb
+++ bidon_back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_30_135323) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_134627) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -154,6 +154,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_135323) do
     t.bigint "app_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "priority", default: 0, null: false
     t.index ["app_id"], name: "index_segments_on_app_id"
   end
 
