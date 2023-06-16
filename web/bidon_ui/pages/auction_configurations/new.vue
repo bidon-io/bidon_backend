632145b5b466881f76bb9c34a5c5d5aec38eb03f
AS <as@bidon.org>
2023-06-16 15:05:13 +0300

    [BDN-XXX] Fix typo on auction config edit page (#190)
    * [BDN-XXX] Fix typo on auction config edit page
    * [BDN-XXX] Fix form submit

--- web/bidon_ui/pages/auction_configurations/new.vue
+++ web/bidon_ui/pages/auction_configurations/new.vue
@@ -2,40 +2,16 @@
   <Toast />
   <PageContainer>
     <NavigationContainer>
-      <NuxtLink to="/auction_configurations/">
-        <Button label="Go back" icon="pi pi-arrow-left" severity="secondary" text />
-      </NuxtLink>
+      <GoBackButton />
     </NavigationContainer>
     <AuctionConfigurationForm :value="resource" @submit="handleSubmit" />
   </PageContainer>
 </template>
 
 <script setup>
-import axios from "@/services/ApiService.js";
-import { useToast } from "primevue/usetoast";
 const resource = {};
-
-const toast = useToast();
-const handleSubmit = (event) => {
-  axios
-    .post("/auction_configurations", event)
-    .then(async (response) => {
-      const id = response.data.id;
-      await navigateTo(`/auction_configurations/${id}`);
-
-      toast.add({
-        severity: "success",
-        summary: "Success",
-        detail: "Auction configuration created",
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-      toast.add({
-        severity: "error",
-        summary: "Error",
-        detail: error.message,
-      });
-    });
-};
+const resourcesPath = "/auction_configurations";
+const handleSubmit = useFormSubmit(resourcesPath, "post", "Auction configuration created!", async (id) =>
+  navigateTo(`${resourcesPath}/${id}`)
+);
 </script>
