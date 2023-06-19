2ecc28ba5939896a64c0e8ecf3e63bf294cc7be5
AJ <aj@bidon.org>
2023-06-19 17:34:46 +0200

    [BDN-243] Add segment_id to auction_configurations (#180)
    * [BDN-243] Add segment_id to auction_configurations
    
    * [BDN-243] Add segment to auction_configuration pages
    
    * [BDN-243] Update callback
    
    * [BDN-243] Add None option to segment
    
    * [BDN-243] Add possibility to set null int64 to db
    
    * [BDN-243] fix checks
    
    * [BDN-243] Apply suggestions
    
    * [BDN-243] Add id to dbModel methods

---
+++ internal/db/auction_configuration.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"errors"
+	"gorm.io/gorm"
+)
+
+func (ac *AuctionConfiguration) BeforeSave(tx *gorm.DB) (err error) {
+	// Check if the combination of app_id, ad_type, and segment_id is already taken
+	var count int64
+
+	query := tx.Model(&AuctionConfiguration{}).
+		Where("app_id = ? AND ad_type = ?", ac.AppID, ac.AdType)
+
+	if ac.SegmentID != nil && ac.SegmentID.Valid {
+		query = query.Where("segment_id = ?", ac.SegmentID.Int64)
+	} else {
+		query = query.Where("segment_id IS NULL")
+	}
+
+	query = query.Not(ac.Model.ID).Count(&count)
+
+	if query.Error != nil {
+		return query.Error
+	}
+
+	if count > 0 {
+		return errors.New("the combination of app_id, ad_type, and segment_id already exists")
+	}
+
+	return nil
+}
