0ac85aa0054856880bfd3af20aecfc321e3e5f5f
ID <id@bidon.org>
2023-06-26 12:39:18 +0200

    [BDN-225] Move /config endpoint to go (#205)

---
+++ internal/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"context"
+
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+)
+
+type Adapters map[adapter.Key]map[string]any
+
+func NewAdapters(keys []adapter.Key) Adapters {
+	adapters := make(Adapters, len(keys))
+	for _, key := range keys {
+		// explicitly initialize with empty maps. nil maps are serialized to `null` in json, empty maps are serialized to `{}`
+		adapters[key] = map[string]any{}
+	}
+	return adapters
+}
+
+type AdaptersBuilder struct {
+	AppDemandProfileFetcher AppDemandProfileFetcher
+}
+
+//go:generate go run -mod=mod github.com/matryer/moq@latest -out mocks_test.go . AppDemandProfileFetcher
+
+type AppDemandProfileFetcher interface {
+	// Fetch is used get one profile per adapter key, if present
+	Fetch(ctx context.Context, appID int64, adapterKeys []adapter.Key) ([]AppDemandProfile, error)
+}
+
+type AppDemandProfile struct {
+	AdapterKey   adapter.Key
+	AccountExtra map[string]any
+}
+
+func (b *AdaptersBuilder) Build(ctx context.Context, appID int64, adapterKeys []adapter.Key) (Adapters, error) {
+	profiles, err := b.AppDemandProfileFetcher.Fetch(ctx, appID, adapterKeys)
+	if err != nil {
+		return nil, err
+	}
+
+	adapters := NewAdapters(adapterKeys)
+
+	for _, profile := range profiles {
+		key := profile.AdapterKey
+		extra := profile.AccountExtra
+		switch key {
+		case adapter.ApplovinKey:
+			adapters[key]["app_key"] = extra["api_key"] // notice the "app" and "api" difference
+		case adapter.BidmachineKey:
+			adapters[key]["seller_id"] = extra["seller_id"]
+			adapters[key]["endpoint"] = extra["endpoint"]
+			adapters[key]["mediation_config"] = extra["mediation_config"]
+		default:
+			adapters[key] = extra
+		}
+	}
+
+	return adapters, nil
+}
---
+++ internal/config/store/app_demand_profile_fetcher.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"context"
+
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/config"
+	"github.com/bidon-io/bidon-backend/internal/db"
+)
+
+type AppDemandProfileFetcher struct {
+	DB *db.DB
+}
+
+func (f *AppDemandProfileFetcher) Fetch(ctx context.Context, appID int64, adapterKeys []adapter.Key) ([]config.AppDemandProfile, error) {
+	var dbProfiles []db.AppDemandProfile
+
+	err := f.DB.
+		WithContext(ctx).
+		Select("app_demand_profiles.id").
+		Where("app_id", appID).
+		InnerJoins("Account", f.DB.Select("id", "extra")).
+		InnerJoins("Account.DemandSource", f.DB.Select("api_key").Where(map[string]any{"api_key": adapterKeys})).
+		Find(&dbProfiles).
+		Error
+	if err != nil {
+		return nil, err
+	}
+
+	profiles := make([]config.AppDemandProfile, len(dbProfiles))
+	for i, dbProfile := range dbProfiles {
+		profile := &profiles[i]
+
+		profile.AdapterKey = adapter.Key(dbProfile.Account.DemandSource.APIKey)
+		profile.AccountExtra = dbProfile.Account.Extra
+	}
+
+	return profiles, nil
+}
