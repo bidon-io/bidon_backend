3bd4c8ca4b6987483710f8d299b298058c17a4d7
AJ <aj@bidon.org>
2023-07-03 12:19:24 +0200

    [BDN-245] Add segment matching (#200)
    * [BDN-245] Add segment matching
    
    * [BDN-245] Order by priority + segment in response
    
    * [BDN-245] Geocoder draft logic
    
    * [BDN-245] Fix specs
    
    * [BDN-245] Move segments logic to auction_handler
    
    * [BDN-245] Use geocoder in AuctionHandler
    
    * [BDN-245] Add maxminddb file path
    
    * [BDN-245] Fix dependencies
    
    * [BDN-245] Update specs
    
    * [BDN-245] Update specs
    
    * [BDN-245] Fix geocoder tests
    
    * [BDN-245] Add segment to config
    
    * [BDN-245] Update according to remarks
    
    * [BDN-245] Update PR according to remarks
    
    * [BDN-245] Update dependecies
    
    * Patch geocoding (#217)
    
    * [BDN-245] Set default country code for empty geo body
    
    * [BDN-245] Remove SegmentFetcher interface from BaseHandler
    
    * [BDN-245] Fetch default configuration for abscent segment
    
    * [BDN-245] Remove segment_id order
    
    ---------

---
+++ internal/segment/store/store.go
@@ -0,0 +1,33 @@
+package store
+
+import (
+	"context"
+	"github.com/bidon-io/bidon-backend/internal/db"
+	"github.com/bidon-io/bidon-backend/internal/segment"
+)
+
+type SegmentFetcher struct {
+	DB *db.DB
+}
+
+func (f *SegmentFetcher) Fetch(ctx context.Context, appID int64) ([]segment.Segment, error) {
+	var dbSegments []db.Segment
+	var sgmnts []segment.Segment
+
+	err := f.DB.WithContext(ctx).
+		Where("app_id = ? AND enabled", appID).
+		Order("priority ASC").
+		Find(&dbSegments).Error
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dbSegment := range dbSegments {
+		sgmnts = append(sgmnts, segment.Segment{
+			ID:      dbSegment.ID,
+			Filters: dbSegment.Filters,
+		})
+	}
+
+	return sgmnts, nil
+}
