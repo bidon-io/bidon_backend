0bd086ec233ab1bd90d530714a6786526af6ba56
AS <as@bidon.org>
2023-07-07 15:32:07 +0200

    [BDN-288] Add nested resources for Auction Config (#225)
    * [BDN-288] Add nested resources for Auction Config

--- internal/db/dbtest/dbtest.go
+++ internal/db/dbtest/dbtest.go
@@ -2,12 +2,14 @@
 package dbtest
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 	"testing"
 
 	"github.com/bidon-io/bidon-backend/internal/db"
+	"github.com/bidon-io/bidon-backend/internal/segment"
 	"github.com/joho/godotenv"
 )
 
@@ -33,6 +35,78 @@ func Prepare() *db.DB {
 	}
 
 	return testDB
+}
+
+func CreateApp(t *testing.T, tx *db.DB, index int, user *db.User) *db.App {
+	t.Helper()
+
+	if user == nil {
+		user = CreateUser(t, tx, index)
+	}
+
+	app := &db.App{
+		UserID:     user.ID,
+		PlatformID: db.AndroidPlatformID,
+		HumanName:  "Test App",
+		PackageName: sql.NullString{
+			String: fmt.Sprintf("app.package%d", index),
+			Valid:  true,
+		},
+		AppKey: sql.NullString{
+			String: fmt.Sprintf("appkey%d", index),
+			Valid:  true,
+		},
+		Settings: make(map[string]any),
+	}
+
+	if err := tx.Create(app).Error; err != nil {
+		t.Fatalf("Failed to create app: %v", app)
+	}
+	return app
+}
+
+func CreateAppsList(t *testing.T, tx *db.DB, count int) []*db.App {
+	t.Helper()
+
+	apps := make([]*db.App, count)
+	for i := range apps {
+		apps[i] = CreateApp(t, tx, i, nil)
+	}
+
+	return apps
+}
+
+func CreateSegment(t *testing.T, tx *db.DB, index int, app *db.App) *db.Segment {
+	t.Helper()
+
+	if app == nil {
+		app = CreateApp(t, tx, index, nil)
+	}
+
+	segment := &db.Segment{
+		Name:        "Test Segment",
+		Description: "description",
+		Filters:     []segment.Filter{},
+		Enabled:     new(bool),
+		AppID:       app.ID,
+		Priority:    0,
+	}
+
+	if err := tx.Create(segment).Error; err != nil {
+		t.Fatalf("Failed to create segment: %v", err)
+	}
+	return segment
+}
+
+func CreateSegmentsList(t *testing.T, tx *db.DB, count int) []*db.Segment {
+	t.Helper()
+
+	segments := make([]*db.Segment, count)
+	for i := range segments {
+		segments[i] = CreateSegment(t, tx, i, nil)
+	}
+
+	return segments
 }
 
 func CreateUser(t *testing.T, tx *db.DB, index int) *db.User {
