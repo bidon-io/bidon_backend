26891d2fd4a5287bd0c54abcd3e285662aaf7480
AJ <aj@bidon.org>
2023-07-12 16:10:48 +0200

    [BDN-XXX] Fix segment_id in AC form (#233)

--- web/bidon_ui/components/auction_configurations/AuctionConfigurationForm.vue
+++ web/bidon_ui/components/auction_configurations/AuctionConfigurationForm.vue
@@ -49,7 +49,7 @@ const { errors, useFieldModel, handleSubmit } = useForm({
     adType: yup.string().required().label("AdType"),
     pricefloor: yup.number().positive().required().label("Pricefloor"),
     rounds: yup.array().required(),
-    appId: yup.number().required().label("Segment Id"),
+    segmentId: yup.number().required().label("Segment Id"),
     externalWinNotifications: yup.boolean(),
   }),
   initialValues: {
