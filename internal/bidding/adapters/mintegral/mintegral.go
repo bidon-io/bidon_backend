67014f89edc256d35aafce4436c95f221e475835
AJ <aj@bidon.org>
2023-07-28 14:17:52 +0200

    [BDN-308] Add Mintegral Network Adapter (#251)
    * [BDN-305] Add bigoads adapter logic
    
    * [BDN-311] Use LineItemsMatcher in bidding (#252)
    
    * [BDN-311] Use LineItemsMatcher in bidding
    
    * [BDN-308] Add Mintegral Network Adapter
    
    * [BDN-308] Update size formats
    
    * [BDN-305] Add bigoads adapter logic
    
    * [BDN-311] Use LineItemsMatcher in bidding (#252)
    
    * [BDN-311] Use LineItemsMatcher in bidding
    
    * [BDN-305] Add Tests
    
    * [BDN-305] Add Tests
    
    * [BDN-308] Add test for Mintegral adapter
    
    ---------
    
    Co-authored-by: Alexandr Sikorsky <44235041+SuperSus@users.noreply.github.com>
    Co-authored-by: Alexander Sikorsky <alexandr.sikorsky@appodeal.com>

---
+++ internal/bidding/adapters/mintegral/mintegral.go
@@ -0,0 +1,244 @@
+package mintegral
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/bidon-io/bidon-backend/internal/ad"
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/bidding/adapters"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
+	"github.com/gofrs/uuid/v5"
+	"github.com/prebid/openrtb/v19/adcom1"
+	"github.com/prebid/openrtb/v19/openrtb2"
+)
+
+type MintegralAdapter struct {
+	SellerID string
+	Endpoint string
+	AppID    string
+	TagID    string
+}
+
+var bannerFormats = map[string][2]int64{
+	"BANNER":      {320, 50},
+	"LEADERBOARD": {320, 50},
+	"MREC":        {300, 250},
+	"ADAPTIVE":    {320, 50},
+	"":            {320, 50}, // Default
+}
+
+var fullscreenFormats = map[string][2]int64{
+	"PHONE":  {320, 480},
+	"TABLET": {320, 480},
+}
+
+func (a *MintegralAdapter) banner(br *schema.BiddingRequest) *openrtb2.Imp {
+	size := bannerFormats[string(br.Imp.Format())]
+	w, h := size[0], size[1]
+	if !br.Imp.IsPortrait() {
+		w, h = h, w
+	}
+	return &openrtb2.Imp{
+		Instl: 0,
+		Banner: &openrtb2.Banner{
+			W:   &w,
+			H:   &h,
+			Pos: adcom1.PositionAboveFold.Ptr(),
+		},
+	}
+}
+
+func (a *MintegralAdapter) interstitial(br *schema.BiddingRequest) *openrtb2.Imp {
+	size := fullscreenFormats[string(br.Device.Type)]
+	w, h := size[0], size[1]
+	if !br.Imp.IsPortrait() {
+		w, h = h, w
+	}
+	return &openrtb2.Imp{
+		Instl: 1,
+		Banner: &openrtb2.Banner{
+			W:   &w,
+			H:   &h,
+			Pos: adcom1.PositionFullScreen.Ptr(),
+		},
+	}
+}
+
+func (a *MintegralAdapter) rewarded(br *schema.BiddingRequest) *openrtb2.Imp {
+	size := fullscreenFormats[string(br.Device.Type)]
+	w, h := size[0], size[1]
+	if !br.Imp.IsPortrait() {
+		w, h = h, w
+	}
+	return &openrtb2.Imp{
+		Instl: 0,
+		Video: &openrtb2.Video{
+			W:     w,
+			H:     h,
+			MIMEs: []string{"video/mp4"},
+		},
+		Ext: json.RawMessage(`{"is_rewarded": true}`),
+	}
+}
+
+func (a *MintegralAdapter) CreateRequest(request openrtb2.BidRequest, br *schema.BiddingRequest) (openrtb2.BidRequest, []error) {
+	var errs []error
+	secure := int8(1)
+
+	var imp *openrtb2.Imp
+	switch br.Imp.Type() {
+	case ad.BannerType:
+		imp = a.banner(br)
+	case ad.InterstitialType:
+		imp = a.interstitial(br)
+	case ad.RewardedType:
+		imp = a.rewarded(br)
+	default:
+		return request, []error{errors.New("unknown impression type")}
+	}
+
+	impId, _ := uuid.NewV4()
+	imp.ID = impId.String()
+
+	if a.TagID == "" {
+		return request, []error{errors.New("TagID is empty")}
+	}
+	imp.TagID = a.TagID
+
+	imp.DisplayManager = string(adapter.MintegralKey)
+	imp.DisplayManagerVer = br.Adapters[adapter.MintegralKey].SDKVersion
+	imp.Secure = &secure
+	imp.BidFloor = br.Imp.BidFloor
+	imp.BidFloorCur = "USD"
+
+	request.Imp = []openrtb2.Imp{*imp}
+	request.Cur = []string{"USD"}
+	request.User = &openrtb2.User{
+		BuyerUID: br.Imp.Demands[adapter.MintegralKey]["token"].(string),
+	}
+	request.App.Publisher.ID = a.SellerID
+	request.App.ID = a.AppID
+
+	appExtStructure := &map[string]interface{}{}
+	if br.Imp.IsPortrait() {
+		(*appExtStructure)["orientation"] = 1
+	} else {
+		(*appExtStructure)["orientation"] = 2
+	}
+	raw, _ := json.Marshal(appExtStructure)
+	request.App.Ext = raw
+
+	return request, errs
+}
+
+func (a *MintegralAdapter) ExecuteRequest(ctx context.Context, client *http.Client, request openrtb2.BidRequest) *adapters.DemandResponse {
+	dr := &adapters.DemandResponse{
+		DemandID: adapter.MintegralKey,
+	}
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	dr.RawRequest = string(requestBody)
+
+	url := "http://hb.rayjump.com/bid"
+	httpReq, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+	// It's important to set openrtb header to 2.5
+	httpReq.Header.Add("openrtb", "2.5")
+
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
+		if err == context.DeadlineExceeded {
+			fmt.Println("Timeout")
+			// TODO: Send Timeout Notification if bidder support, eg FB
+		}
+		dr.Error = err
+		return dr
+	}
+
+	respBody, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	defer httpResp.Body.Close()
+	dr.RawResponse = string(respBody)
+	dr.Status = httpResp.StatusCode
+
+	return dr
+}
+
+func (a *MintegralAdapter) ParseBids(dr *adapters.DemandResponse) (*adapters.DemandResponse, error) {
+	switch dr.Status {
+	case http.StatusNoContent:
+		return dr, nil
+	case http.StatusServiceUnavailable:
+		fallthrough
+	case http.StatusBadRequest:
+		fallthrough
+	case http.StatusUnauthorized:
+		fallthrough
+	case http.StatusForbidden:
+		return dr, fmt.Errorf("unauthorized request: " + strconv.Itoa(dr.Status))
+	case http.StatusOK:
+		break
+	default:
+		return dr, fmt.Errorf("unexpected status code: " + strconv.Itoa(dr.Status))
+	}
+
+	var bidResponse openrtb2.BidResponse
+	err := json.Unmarshal([]byte(dr.RawResponse), &bidResponse)
+	if err != nil {
+		return dr, err
+	}
+
+	seat := bidResponse.SeatBid[0]
+	bid := seat.Bid[0]
+
+	dr.Bid = &adapters.BidDemandResponse{
+		ID:       bid.ID,
+		ImpID:    bid.ImpID,
+		Price:    bid.Price,
+		Payload:  bid.AdM,
+		DemandID: adapter.MintegralKey,
+		AdID:     bid.AdID,
+		SeatID:   seat.Seat,
+		LURL:     bid.LURL,
+		NURL:     bid.NURL,
+		BURL:     bid.BURL,
+	}
+
+	return dr, nil
+}
+
+// Builder builds a new instance of the Mintegral adapter for the given bidder with the given config.
+func Builder(cfg adapter.Config, client *http.Client) (adapters.Bidder, error) {
+	mCfg := cfg[adapter.MintegralKey]
+
+	adpt := &MintegralAdapter{
+		Endpoint: mCfg["endpoint"].(string),
+		SellerID: mCfg["seller_id"].(string),
+		AppID:    mCfg["app_id"].(string),
+		TagID:    mCfg["tag_id"].(string),
+	}
+
+	bidder := adapters.Bidder{
+		Adapter: adpt,
+		Client:  client,
+	}
+
+	return bidder, nil
+}
---
+++ internal/bidding/adapters/mintegral/mintegral_test.go
@@ -0,0 +1,230 @@
+package mintegral_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/bidon-io/bidon-backend/internal/ad"
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/bidding/adapters/mintegral"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
+	"github.com/google/go-cmp/cmp"
+	"github.com/prebid/openrtb/v19/adcom1"
+	"github.com/prebid/openrtb/v19/openrtb2"
+)
+
+type createRequestTestParams struct {
+	BaseBidRequest openrtb2.BidRequest
+	Br             *schema.BiddingRequest
+}
+
+type createRequestTestOutput struct {
+	Request openrtb2.BidRequest
+	Err     []error
+}
+
+func ptr[T any](t T) *T {
+	return &t
+}
+
+func buildBaseRequest() openrtb2.BidRequest {
+	return openrtb2.BidRequest{
+		App: &openrtb2.App{
+			Publisher: &openrtb2.Publisher{},
+			Ext:       json.RawMessage(`{"orientation":1}`),
+		},
+	}
+}
+
+func buildTestParams(imp schema.Imp) createRequestTestParams {
+	request := buildBaseRequest()
+
+	br := schema.BiddingRequest{
+		Adapters: schema.Adapters{
+			"mintegral": schema.Adapter{
+				Version:    "1.0.0",
+				SDKVersion: "1.0.0",
+			},
+		},
+		Imp: schema.Imp{
+			Demands: map[adapter.Key]map[string]any{
+				adapter.MintegralKey: {
+					"token": "token",
+				},
+			},
+			Orientation: "PORTRAIT",
+		},
+		BaseRequest: schema.BaseRequest{
+			Device: schema.Device{
+				Type: "PHONE",
+			},
+		},
+	}
+
+	if imp.Banner != nil {
+		br.Imp.Banner = imp.Banner
+	}
+	if imp.Interstitial != nil {
+		br.Imp.Interstitial = imp.Interstitial
+	}
+	if imp.Rewarded != nil {
+		br.Imp.Rewarded = imp.Rewarded
+	}
+
+	return createRequestTestParams{
+		BaseBidRequest: request,
+		Br:             &br,
+	}
+}
+
+func buildWantRequest(imp openrtb2.Imp) openrtb2.BidRequest {
+	request := openrtb2.BidRequest{
+		App: &openrtb2.App{
+			ID:        "10182906",
+			Publisher: &openrtb2.Publisher{ID: "1"},
+			Ext:       json.RawMessage(`{"orientation":1}`),
+		},
+		User: &openrtb2.User{BuyerUID: "token"},
+		Cur:  []string{"USD"},
+		Imp: []openrtb2.Imp{
+			{
+				ID:                "1",
+				DisplayManager:    "mintegral",
+				DisplayManagerVer: "1.0.0",
+				TagID:             "10182906-10192212",
+				BidFloorCur:       "USD",
+				Secure:            ptr(int8(1)),
+			},
+		},
+	}
+	if imp.Banner != nil {
+		request.Imp[0].Banner = imp.Banner
+	}
+	if imp.Video != nil {
+		request.Imp[0].Video = imp.Video
+	}
+	if imp.Instl != 0 {
+		request.Imp[0].Instl = imp.Instl
+	}
+	if imp.Ext != nil {
+		request.Imp[0].Ext = imp.Ext
+	}
+
+	return request
+}
+
+func TestMintegral_CreateRequestTest(t *testing.T) {
+	testCases := []struct {
+		name   string
+		params createRequestTestParams
+		want   createRequestTestOutput
+	}{
+		{
+			name: "Banner MREC",
+			params: buildTestParams(
+				schema.Imp{
+					Banner: &schema.BannerAdObject{
+						Format: ad.MRECFormat,
+					},
+				},
+			),
+			want: createRequestTestOutput{
+				Request: buildWantRequest(openrtb2.Imp{
+					Instl: 0,
+					Banner: &openrtb2.Banner{
+						W:   ptr(int64(300)),
+						H:   ptr(int64(250)),
+						Pos: adcom1.PositionAboveFold.Ptr(),
+					},
+					Ext: nil,
+				}),
+				Err: nil,
+			},
+		},
+		{
+			name: "Banner BANNER",
+			params: buildTestParams(
+				schema.Imp{
+					Banner: &schema.BannerAdObject{
+						Format: ad.BannerFormat,
+					},
+				},
+			),
+			want: createRequestTestOutput{
+				Request: buildWantRequest(openrtb2.Imp{
+					Instl: 0,
+					Banner: &openrtb2.Banner{
+						W:   ptr(int64(320)),
+						H:   ptr(int64(50)),
+						Pos: adcom1.PositionAboveFold.Ptr(),
+					},
+					Ext: nil,
+				}),
+				Err: nil,
+			},
+		},
+		{
+			name: "Interstitial",
+			params: buildTestParams(
+				schema.Imp{
+					Interstitial: &schema.InterstitialAdObject{},
+				},
+			),
+			want: createRequestTestOutput{
+				Request: buildWantRequest(openrtb2.Imp{
+					Instl: 1,
+					Banner: &openrtb2.Banner{
+						W:   ptr(int64(320)),
+						H:   ptr(int64(480)),
+						Pos: adcom1.PositionFullScreen.Ptr(),
+					},
+					Ext: nil,
+				}),
+				Err: nil,
+			},
+		},
+		{
+			name: "Rewarded",
+			params: buildTestParams(
+				schema.Imp{
+					Rewarded: &schema.RewardedAdObject{},
+				},
+			),
+			want: createRequestTestOutput{
+				Request: buildWantRequest(openrtb2.Imp{
+					Instl: 0,
+					Video: &openrtb2.Video{
+						MIMEs: []string{"video/mp4"},
+						W:     int64(320),
+						H:     int64(480),
+					},
+					Ext: json.RawMessage(`{"is_rewarded": true}`),
+				}),
+				Err: nil,
+			},
+		},
+	}
+
+	adapter := &mintegral.MintegralAdapter{
+		SellerID: "1",
+		Endpoint: "https://mintegral.com",
+		AppID:    "10182906",
+		TagID:    "10182906-10192212",
+	}
+
+	for _, tC := range testCases {
+		request, err := adapter.CreateRequest(tC.params.BaseBidRequest, tC.params.Br)
+		if err == nil {
+			request.Imp[0].ID = "1" // ommit random uuid
+		}
+		got := createRequestTestOutput{
+			Request: request,
+			Err:     err,
+		}
+		if diff := cmp.Diff(tC.want, got, cmp.Comparer(func(x, y error) bool {
+			return x.Error() == y.Error()
+		})); diff != "" {
+			t.Errorf("%s: adapter.CreateRequest(ctx, %v, %v) mismatch (-want, +got):\n%s", tC.name, tC.params.BaseBidRequest, tC.params.Br, diff)
+		}
+	}
+}
