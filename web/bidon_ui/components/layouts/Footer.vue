578b3a9dc2b6d04b68b5b30692dcaec59dd99e1d
AS <as@bidon.org>
2023-08-04 13:36:14 +0200

    [BDN-327] UI Enhancements (#277)
    * [BDN-327] UI Enhancments
    * [BDN-327] Add app, account filters
    * [BDN-327] Add app associated resources links

---
+++ web/bidon_ui/components/layouts/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="bg-gray-200 py-4">
+    <p class="text-center text-xs text-gray-600 py-2">
+      &copy; BidON. All rights reserved.
+    </p>
+  </footer>
+</template>
---
+++ web/bidon_ui/components/resources/AssociatedResourcesLink.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink :to="path" class="text-blue-500">
+    {{ label }}
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  link: AssociatedResourcesLink;
+  data: object;
+}>();
+const { label, path } = props.link.extractLinkData(props.data);
+</script>
