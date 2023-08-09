fdd7508faefabdaeb85387f3dfc53bc6a3b04d21
AJ <aj@bidon.org>
2023-08-09 14:37:26 +0200

    [BDN-336] Implement CRUD UI to the least used resources (Demand Source → Country → User) (#289)

---
+++ web/bidon_ui/pages/countries/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <CreateResourceButton label="New Country" :resources-path="resourcesPath" />
+  <ResourcesTable :columns="columns" :resources-path="resourcesPath" />
+</template>
+
+<script setup>
+import { ResourceTableFields } from "@/constants";
+
+const columns = [
+  ResourceTableFields.Id,
+  { field: "humanName", header: "Human Name" },
+  { field: "alpha2Code", header: "Alpha 2 Code" },
+  { field: "alpha3Code", header: "Alpha 3 Code" },
+];
+const resourcesPath = "/countries";
+</script>
