ad89803fd1cde59254937060584879792fc16d3a
KI <ki@bidon.org>
2023-08-11 10:33:57 +0200

    [BDN-334] Filter bids with price lower than floor (#290)
    * [BDN-334] Filter bids with price lower than floor
    
    * [BDN-XXX] Add tests for bidding handler

--- internal/notification/auction_result.go
+++ internal/notification/auction_result.go
@@ -41,7 +41,6 @@ type Bid struct {
 	ID        string      `json:"id"`
 	ImpID     string      `json:"impid"`
 	Price     float64     `json:"price"`
-	Payload   string      `json:"payload"`
 	DemandID  adapter.Key `json:"demand_id"`
 	AdID      string      `json:"adid"`
 	SeatID    string      `json:"seatid"`
