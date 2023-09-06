6a4a23b57e799d8a0853d0a1a90cf3486f5ed90e
AS <as@bidon.org>
2023-09-06 15:16:53 +0200

    [BDN-370] Create/Update user with Password and IsAdmin (#308)
    * [BDN-370] Create/Update user with Password and IsAdmin

--- internal/admin/store/user_repo.go
+++ internal/admin/store/user_repo.go
@@ -7,34 +7,37 @@ import (
 
 type UserRepo = resourceRepo[admin.User, admin.UserAttrs, db.User]
 
-func NewUserRepo(db *db.DB) *UserRepo {
+type userMapper struct{}
+
+func NewUserRepo(d *db.DB) *UserRepo {
 	return &UserRepo{
-		db:           db,
+		db:           d,
 		mapper:       userMapper{},
 		associations: []string{},
 	}
 }
 
-type userMapper struct{}
-
 //lint:ignore U1000 this method is used by generic struct
 func (m userMapper) dbModel(u *admin.UserAttrs, id int64) *db.User {
-	return &db.User{
-		Model: db.Model{ID: id},
-		Email: u.Email,
+	du := &db.User{
+		Model:   db.Model{ID: id},
+		Email:   u.Email,
+		IsAdmin: u.IsAdmin,
 	}
+
+	if u.Password != "" {
+		passwordHash, _ := db.HashPassword(u.Password)
+		du.PasswordHash = passwordHash
+	}
+
+	return du
 }
 
 //lint:ignore U1000 this method is used by generic struct
 func (m userMapper) resource(u *db.User) admin.User {
 	return admin.User{
-		ID:        u.ID,
-		UserAttrs: m.resourceAttrs(u),
-	}
-}
-
-func (m userMapper) resourceAttrs(u *db.User) admin.UserAttrs {
-	return admin.UserAttrs{
-		Email: u.Email,
+		ID:      u.ID,
+		Email:   u.Email,
+		IsAdmin: u.IsAdmin,
 	}
 }
