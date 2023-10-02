8826846b032e6da07ea50cce50ccc02f59a33543
AJ <aj@bidon.org>
2023-10-02 16:39:50 +0200

    [BDN-430] Enhance The App Demand Profile Form (#344)

--- web/bidon_ui/pages/app_demand_profiles/[id]/edit.vue
+++ web/bidon_ui/pages/app_demand_profiles/[id]/edit.vue
@@ -6,6 +6,7 @@
     <AppDemandProfileForm
       v-if="isReady"
       :value="resource"
+      :submit-error="error"
       @submit="handleSubmit"
     />
   </PageContainer>
@@ -24,8 +25,10 @@ const { state: resource, isReady } = useAsyncState(async () => {
   return response.data;
 });
 
+const error = ref(null);
 const handleSubmit = useUpdateResource({
   path: resourcePath,
   message: "App Demand Profile Updated!",
+  onError: async (e) => (error.value = e),
 });
 </script>
