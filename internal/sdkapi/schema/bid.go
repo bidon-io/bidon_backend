632773964d8ec60982dc0c324a2f67dca16863d6
KI <ki@bidon.org>
2023-10-13 14:27:56 +0200

    [BDN-375] Add round_idx and bid_type fields (#363)
    * [BDN-375] Add round_idx field to bid object
    
    * [BDN-373] Add bit_type to stats

--- internal/sdkapi/schema/bid.go
+++ internal/sdkapi/schema/bid.go
@@ -9,12 +9,18 @@ type Bid struct {
 	ImpID                   string                `json:"imp_id" validate:"required"`
 	DemandID                string                `json:"demand_id" validate:"required"`
 	RoundID                 string                `json:"round_id"`
+	RoundIndex              int                   `json:"round_idx"`
 	AdUnitID                string                `json:"ad_unit_id"`
 	LineItemUID             string                `json:"line_item_uid"`
 	ECPM                    float64               `json:"ecpm" validate:"required"`
+	BidType                 string                `json:"bid_type" validate:"omitempty,oneof=rtb cpm"`
 	Banner                  *BannerAdObject       `json:"banner"`
 	Interstitial            *InterstitialAdObject `json:"interstitial"`
 	Rewarded                *RewardedAdObject     `json:"rewarded"`
+}
+
+func (b Bid) IsBidding() bool {
+	return b.BidType == "rtb"
 }
 
 func (b Bid) Map() map[string]any {
@@ -34,9 +40,12 @@ func (b Bid) Map() map[string]any {
 		"imp_id":                    b.ImpID,
 		"demand_id":                 b.DemandID,
 		"round_id":                  b.RoundID,
+		"round_number":              b.RoundIndex,
 		"ad_unit_id":                b.AdUnitID,
 		"line_item_uid":             lineItemUID,
 		"ecpm":                      b.ECPM,
+		"bid_type":                  b.BidType,
+		"bidding":                   b.IsBidding(),
 	}
 
 	if b.Banner != nil {
