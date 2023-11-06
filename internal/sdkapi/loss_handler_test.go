00f369359c9e155b6148280d9e97cf21a81277ed
KI <ki@bidon.org>
2023-11-06 19:21:56 +0100

    [BDN-502] Turn on cache for apps (#398)

--- internal/sdkapi/loss_handler_test.go
+++ internal/sdkapi/loss_handler_test.go
@@ -29,7 +29,7 @@ func SetupLossHandler() sdkapi.LossHandler {
 		return nil
 	}
 	appFetcher := &mocks.AppFetcherMock{
-		FetchFunc: func(ctx context.Context, appKey string, appBundle string) (sdkapi.App, error) {
+		FetchCachedFunc: func(ctx context.Context, appKey string, appBundle string) (sdkapi.App, error) {
 			return app, nil
 		},
 	}
