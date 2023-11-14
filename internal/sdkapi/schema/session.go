c1c9e9c8b14f83bb4155a081a2bf50c22093590f
KI <ki@bidon.org>
2023-11-14 16:41:04 +0100

    [BDN-XXX] Add session object to kafka events (#411)

--- internal/sdkapi/schema/session.go
+++ internal/sdkapi/schema/session.go
@@ -1,21 +1,21 @@
 package schema
 
 type Session struct {
-	ID                        string   `json:"id" validate:"required,uuid"`
-	LaunchTS                  int      `json:"launch_ts" validate:"required"`
-	LaunchMonotonicTS         int      `json:"launch_monotonic_ts" validate:"required"`
-	StartTS                   int      `json:"start_ts" validate:"required"`
-	StartMonotonicTS          int      `json:"start_monotonic_ts" validate:"required"`
-	TS                        int      `json:"ts" validate:"required"`
-	MonotonicTS               int      `json:"monotonic_ts" validate:"required"`
-	MemoryWarningsTS          []int    `json:"memory_warnings_ts" validate:"required"`
-	MemoryWarningsMonotonicTS []int    `json:"memory_warnings_monotonic_ts" validate:"required"`
-	RAMUsed                   int      `json:"ram_used" validate:"required"`
-	RAMSize                   int      `json:"ram_size" validate:"required"`
-	StorageFree               int      `json:"storage_free" validate:"required"`
-	StorageUsed               int      `json:"storage_used" validate:"required"`
-	Battery                   float64  `json:"battery" validate:"required"`
-	CPUUsage                  *float64 `json:"cpu_usage" validate:"required"`
+	ID                        string  `json:"id" validate:"required,uuid"`
+	LaunchTS                  int     `json:"launch_ts" validate:"required"`
+	LaunchMonotonicTS         int     `json:"launch_monotonic_ts" validate:"required"`
+	StartTS                   int     `json:"start_ts" validate:"required"`
+	StartMonotonicTS          int     `json:"start_monotonic_ts" validate:"required"`
+	TS                        int     `json:"ts" validate:"required"`
+	MonotonicTS               int     `json:"monotonic_ts" validate:"required"`
+	MemoryWarningsTS          []int   `json:"memory_warnings_ts" validate:"required"`
+	MemoryWarningsMonotonicTS []int   `json:"memory_warnings_monotonic_ts" validate:"required"`
+	RAMUsed                   int     `json:"ram_used" validate:"required"`
+	RAMSize                   int     `json:"ram_size" validate:"required"`
+	StorageFree               int     `json:"storage_free" validate:"required"`
+	StorageUsed               int     `json:"storage_used" validate:"required"`
+	Battery                   float64 `json:"battery" validate:"required"`
+	CPUUsage                  float64 `json:"cpu_usage" validate:"required"`
 }
 
 func (s Session) Map() map[string]any {
