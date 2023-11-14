b3080077d15e2361aece8610a18948575bb8fff2
KI <ki@bidon.org>
2023-11-14 16:41:04 +0100

    [BDN-XXX] Add session object to kafka events (#411)

--- internal/sdkapi/event/event_test.go
+++ internal/sdkapi/event/event_test.go
@@ -2,6 +2,8 @@ package event
 
 import (
 	"errors"
+	"github.com/bidon-io/bidon-backend/config"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
 	"testing"
 
 	"github.com/bidon-io/bidon-backend/internal/sdkapi/geocoder"
@@ -207,5 +209,25 @@ func TestPrepareEventPayload(t *testing.T) {
 				t.Errorf("%v: prepareEventPayload() got error %q, want error %q", test.name, err, test.want.err)
 			}
 		})
+	}
+}
+
+func TestNewRequest(t *testing.T) {
+	request := &schema.BaseRequest{}
+	adRequestParams := AdRequestParams{}
+	geoData := geocoder.GeoData{IPString: "8.8.8.8", CountryCode: "US", CountryID: 1}
+	event := NewRequest(request, adRequestParams, geoData)
+
+	testImplementEvent(t, event)
+
+	if event.Topic() != config.AdEventsTopic {
+		t.Errorf("NewRequest: expected topic %v, got %v", config.AdEventsTopic, event.Topic())
+	}
+}
+
+func testImplementEvent(t *testing.T, e any) {
+	_, ok := e.(Event)
+	if !ok {
+		t.Errorf("NewRequest: expected event to implement Event interface")
 	}
 }
