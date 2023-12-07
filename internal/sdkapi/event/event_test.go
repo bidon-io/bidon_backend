47195b07d72f1472bc7e7a9804369cd414125790
AJ <aj@bidon.org>
2023-12-07 11:31:41 +0100

    [BDN-594] Use HardwareVersion for ios model (#456)

--- internal/sdkapi/event/event_test.go
+++ internal/sdkapi/event/event_test.go
@@ -47,3 +47,54 @@ func testImplementEvent(t *testing.T, e any) {
 		t.Errorf("NewAdEvent: expected event to implement Event interface")
 	}
 }
+
+func TestNewAdEventModelField(t *testing.T) {
+	testCases := []struct {
+		name     string
+		deviceOS string
+		model    string
+		hardware string
+		expected string
+	}{
+		{
+			name:     "Android",
+			deviceOS: "android",
+			model:    "Pixel 4",
+			hardware: "qcom",
+			expected: "Pixel 4",
+		},
+		{
+			name:     "iPhone",
+			deviceOS: "iOS",
+			model:    "iPhone",
+			hardware: "iPhone12,1",
+			expected: "iPhone12,1",
+		},
+		{
+			name:     "iPad",
+			deviceOS: "iPadOS",
+			model:    "iPad",
+			hardware: "iPad7,11",
+			expected: "iPad7,11",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			request := &schema.BaseRequest{
+				Device: schema.Device{
+					OS:              tc.deviceOS,
+					Model:           tc.model,
+					HardwareVersion: tc.hardware,
+				},
+			}
+			adRequestParams := AdRequestParams{}
+			geoData := geocoder.GeoData{IPString: "8.8.8.8", CountryCode: "US", CountryID: 1}
+			event := NewAdEvent(request, adRequestParams, geoData)
+
+			if event.Model != tc.expected {
+				t.Errorf("NewAdEvent: expected model %v, got %v", tc.expected, event.Model)
+			}
+		})
+	}
+}
