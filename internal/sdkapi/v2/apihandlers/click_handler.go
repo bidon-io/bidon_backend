7fc5ae890d2993e5e1a04a155726b85b23dc88c5
AJ <aj@bidon.org>
2024-05-31 12:39:02 +0200

    [BDN-651] Change to v1/v2 handlers folders (#464)
    * [BDN-651] Resolve api versioning using middleware and version routers
    
    * [BDN-651] Return common schema
    
    * [BDN-651] Remove versioning middleware
    
    * [BDN-651] Move handlers to v2
    
    * [BDN-651] Fix tests
    
    * [BDN-651] Move AdapterInitConfig to separate file
    
    * [BDN-651] Rename handlers -> apihandlers

---
+++ internal/sdkapi/v2/apihandlers/click_handler.go
@@ -0,0 +1,61 @@
+package apihandlers
+
+import (
+	"fmt"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi"
+	"net/http"
+	"strconv"
+
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/event"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
+	"github.com/labstack/echo/v4"
+)
+
+type ClickHandler struct {
+	*BaseHandler[schema.ClickRequest, *schema.ClickRequest]
+	EventLogger *event.Logger
+}
+
+func (h *ClickHandler) Handle(c echo.Context) error {
+	req, err := h.resolveRequest(c)
+	if err != nil {
+		return err
+	}
+
+	adEvent := prepareClickEvent(req)
+	h.EventLogger.Log(adEvent, func(err error) {
+		sdkapi.LogError(c, fmt.Errorf("log click event: %v", err))
+	})
+
+	return c.JSON(http.StatusOK, map[string]any{"success": true})
+}
+
+func prepareClickEvent(req *request[schema.ClickRequest, *schema.ClickRequest]) *event.AdEvent {
+	bid := req.raw.Bid
+
+	auctionConfigurationUID, err := strconv.ParseInt(bid.AuctionConfigurationUID, 10, 64)
+	if err != nil {
+		auctionConfigurationUID = 0
+	}
+
+	adRequestParams := event.AdRequestParams{
+		EventType:               "click",
+		AdType:                  string(req.raw.AdType),
+		AdFormat:                string(bid.Format()),
+		AuctionID:               bid.AuctionID,
+		AuctionConfigurationID:  bid.AuctionConfigurationID,
+		AuctionConfigurationUID: auctionConfigurationUID,
+		Status:                  "",
+		RoundID:                 bid.RoundID,
+		RoundNumber:             bid.RoundIndex,
+		ImpID:                   bid.ImpID,
+		DemandID:                bid.DemandID,
+		AdUnitUID:               int64(bid.GetAdUnitUID()),
+		AdUnitLabel:             bid.AdUnitLabel,
+		ECPM:                    bid.GetPrice(),
+		PriceFloor:              bid.AuctionPriceFloor,
+		Bidding:                 bid.IsBidding(),
+	}
+
+	return event.NewAdEvent(&req.raw.BaseRequest, adRequestParams, req.geoData)
+}
