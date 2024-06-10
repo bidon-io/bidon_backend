c18698ef1a9610d7086e4b9e957b11056f426d14
AJ <aj@bidon.org>
2024-06-10 11:17:00 +0200

    [BDN-654] Add AuctionConfigurationV2 CRUD (Admin)[Backend] (#484)
    * [BDN-654] Add auction_configuration_v2 repo and admin files
    
    * [BDN-654] Actualize master
    
    * [BDN-654] Fix tests

---
+++ internal/admin/auction_configuration_v2.go
@@ -0,0 +1,153 @@
+package admin
+
+import (
+	"context"
+	"github.com/bidon-io/bidon-backend/internal/ad"
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+)
+
+const AuctionConfigurationV2ResourceKey = "auction_configuration_v2"
+
+type AuctionConfigurationV2Resource struct {
+	*AuctionConfigurationV2
+	Permissions ResourceInstancePermissions `json:"_permissions"`
+}
+
+type AuctionConfigurationV2 struct {
+	ID         int64  `json:"id"`
+	PublicUID  string `json:"public_uid"`
+	AuctionKey string `json:"auction_key"`
+	AuctionConfigurationV2Attrs
+	App     App      `json:"app"`
+	Segment *Segment `json:"segment"`
+}
+
+// AuctionConfigurationV2Attrs is attributes of Configuration. Used to create and update configurations
+type AuctionConfigurationV2Attrs struct {
+	Name                     string        `json:"name"`
+	AppID                    int64         `json:"app_id"`
+	AdType                   ad.Type       `json:"ad_type"`
+	Pricefloor               float64       `json:"pricefloor"`
+	SegmentID                *int64        `json:"segment_id"`
+	ExternalWinNotifications *bool         `json:"external_win_notifications"`
+	Demands                  []adapter.Key `json:"demands"`
+	Bidding                  []adapter.Key `json:"bidding"`
+	AdUnitIDs                []int64       `json:"ad_unit_ids"`
+	Timeout                  int32         `json:"timeout"`
+}
+
+type AuctionConfigurationV2Service struct {
+	*ResourceService[AuctionConfigurationV2Resource, AuctionConfigurationV2, AuctionConfigurationV2Attrs]
+}
+
+func NewAuctionConfigurationV2Service(store Store) *AuctionConfigurationV2Service {
+	s := &AuctionConfigurationV2Service{
+		ResourceService: &ResourceService[AuctionConfigurationV2Resource, AuctionConfigurationV2, AuctionConfigurationV2Attrs]{},
+	}
+
+	s.resourceKey = AuctionConfigurationV2ResourceKey
+
+	s.repo = store.AuctionConfigurationsV2()
+	s.policy = newAuctionConfigurationV2Policy(store)
+
+	s.prepareResource = func(authCtx AuthContext, config *AuctionConfigurationV2) AuctionConfigurationV2Resource {
+		return AuctionConfigurationV2Resource{
+			AuctionConfigurationV2: config,
+			Permissions:            s.policy.instancePermissions(authCtx, config),
+		}
+	}
+
+	return s
+}
+
+type AuctionConfigurationV2Repo interface {
+	AllResourceQuerier[AuctionConfigurationV2]
+	OwnedResourceQuerier[AuctionConfigurationV2]
+	ResourceManipulator[AuctionConfigurationV2, AuctionConfigurationV2Attrs]
+}
+
+type auctionConfigurationV2Policy struct {
+	repo AuctionConfigurationV2Repo
+
+	appPolicy     *appPolicy
+	segmentPolicy *segmentPolicy
+}
+
+func newAuctionConfigurationV2Policy(store Store) *auctionConfigurationV2Policy {
+	return &auctionConfigurationV2Policy{
+		repo: store.AuctionConfigurationsV2(),
+
+		appPolicy:     newAppPolicy(store),
+		segmentPolicy: newSegmentPolicy(store),
+	}
+}
+
+func (p *auctionConfigurationV2Policy) getReadScope(authCtx AuthContext) resourceScope[AuctionConfigurationV2] {
+	return &ownedResourceScope[AuctionConfigurationV2]{
+		repo:    p.repo,
+		authCtx: authCtx,
+	}
+}
+
+func (p *auctionConfigurationV2Policy) getManageScope(authCtx AuthContext) resourceScope[AuctionConfigurationV2] {
+	return &ownedResourceScope[AuctionConfigurationV2]{
+		repo:    p.repo,
+		authCtx: authCtx,
+	}
+}
+
+func (p *auctionConfigurationV2Policy) authorizeCreate(ctx context.Context, authCtx AuthContext, attrs *AuctionConfigurationV2Attrs) error {
+	// Check if user can manage the app.
+	_, err := p.appPolicy.getManageScope(authCtx).find(ctx, attrs.AppID)
+	if err != nil {
+		return err
+	}
+
+	if attrs.SegmentID != nil {
+		// Check if user can read the segment.
+		_, err = p.segmentPolicy.getReadScope(authCtx).find(ctx, *attrs.SegmentID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *auctionConfigurationV2Policy) authorizeUpdate(ctx context.Context, authCtx AuthContext, config *AuctionConfigurationV2, attrs *AuctionConfigurationV2Attrs) error {
+	// If user tries to change the app and app is not the same as before, check if user can manage the new app.
+	if attrs.AppID != 0 && attrs.AppID != config.AppID {
+		_, err := p.appPolicy.getManageScope(authCtx).find(ctx, attrs.AppID)
+		if err != nil {
+			return err
+		}
+	}
+
+	// If user tries to change the segment and segment is not the same as before, check if user can read the new segment.
+	if attrs.SegmentID != nil && *attrs.SegmentID != *config.SegmentID {
+		_, err := p.segmentPolicy.getReadScope(authCtx).find(ctx, *attrs.SegmentID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (p *auctionConfigurationV2Policy) authorizeDelete(_ context.Context, _ AuthContext, _ *AuctionConfigurationV2) error {
+	return nil
+}
+
+func (p *auctionConfigurationV2Policy) permissions(_ AuthContext) ResourcePermissions {
+	return ResourcePermissions{
+		Read:   true,
+		Create: true,
+	}
+}
+
+func (p *auctionConfigurationV2Policy) instancePermissions(_ AuthContext, _ *AuctionConfigurationV2) ResourceInstancePermissions {
+	return ResourceInstancePermissions{
+		Update: true,
+		Delete: true,
+	}
+}
---
+++ internal/admin/store/auction_configuration_v2_repo.go
@@ -0,0 +1,133 @@
+package adminstore
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"math/big"
+	"strconv"
+	"strings"
+
+	"github.com/bidon-io/bidon-backend/internal/admin"
+	"github.com/bidon-io/bidon-backend/internal/db"
+	"gorm.io/gorm"
+)
+
+type AuctionConfigurationV2Repo struct {
+	*resourceRepo[admin.AuctionConfigurationV2, admin.AuctionConfigurationV2Attrs, db.AuctionConfiguration]
+}
+
+func NewAuctionConfigurationV2Repo(d *db.DB) *AuctionConfigurationV2Repo {
+	return &AuctionConfigurationV2Repo{
+		resourceRepo: &resourceRepo[admin.AuctionConfigurationV2, admin.AuctionConfigurationV2Attrs, db.AuctionConfiguration]{
+			db:           d,
+			mapper:       auctionConfigurationV2Mapper{db: d},
+			associations: []string{"App", "Segment"},
+		},
+	}
+}
+
+func (r *AuctionConfigurationV2Repo) List(ctx context.Context) ([]admin.AuctionConfigurationV2, error) {
+	return r.list(ctx, func(db *gorm.DB) *gorm.DB {
+		return db.Where("settings->>'v2' = ?", "true")
+	})
+}
+
+func (r *AuctionConfigurationV2Repo) ListOwnedByUser(ctx context.Context, userID int64) ([]admin.AuctionConfigurationV2, error) {
+	return r.list(ctx, func(db *gorm.DB) *gorm.DB {
+		s := db.Session(&gorm.Session{NewDB: true})
+		return db.InnerJoins("App", s.Select("user_id").Where(map[string]any{"user_id": userID}).Where("settings->>'v2' = ?", "true"))
+	})
+}
+
+func (r *AuctionConfigurationV2Repo) FindOwnedByUser(ctx context.Context, userID int64, id int64) (*admin.AuctionConfigurationV2, error) {
+	return r.find(ctx, id, func(db *gorm.DB) *gorm.DB {
+		s := db.Session(&gorm.Session{NewDB: true})
+		return db.InnerJoins("App", s.Select("user_id").Where(map[string]any{"user_id": userID}).Where("settings->>'v2' = ?", "true"))
+	})
+}
+
+type auctionConfigurationV2Mapper struct {
+	db *db.DB
+}
+
+//lint:ignore U1000 this method is used by generic struct
+func (m auctionConfigurationV2Mapper) dbModel(c *admin.AuctionConfigurationV2Attrs, id int64) *db.AuctionConfiguration {
+	name := sql.NullString{}
+	if c.Name != "" {
+		name.String = c.Name
+		name.Valid = true
+	}
+	segmentID := sql.NullInt64{}
+	if c.SegmentID != nil {
+		segmentID.Int64 = *c.SegmentID
+		segmentID.Valid = true
+	}
+
+	settingsMap := make(map[string]any)
+	if id == 0 {
+		settingsMap["v2"] = true
+	}
+
+	settings, _ := json.Marshal(settingsMap)
+
+	return &db.AuctionConfiguration{
+		ID:                       id,
+		Name:                     name,
+		AppID:                    c.AppID,
+		AdType:                   db.AdTypeFromDomain(c.AdType),
+		Pricefloor:               c.Pricefloor,
+		SegmentID:                &segmentID,
+		ExternalWinNotifications: c.ExternalWinNotifications,
+		Demands:                  db.AdapterKeysToStringArray(c.Demands),
+		Bidding:                  db.AdapterKeysToStringArray(c.Bidding),
+		AdUnitIds:                c.AdUnitIDs,
+		Timeout:                  c.Timeout,
+		Settings:                 settings,
+	}
+}
+
+//lint:ignore U1000 this method is used by generic struct
+func (m auctionConfigurationV2Mapper) resource(c *db.AuctionConfiguration) admin.AuctionConfigurationV2 {
+	var segment *admin.Segment
+	if c.Segment != nil {
+		segment = &admin.Segment{
+			ID:           c.Segment.ID,
+			SegmentAttrs: segmentMapper{}.resourceAttrs(c.Segment),
+		}
+	}
+
+	return admin.AuctionConfigurationV2{
+		ID:                          c.ID,
+		PublicUID:                   strconv.FormatInt(c.PublicUID.Int64, 10),
+		AuctionKey:                  strings.ToUpper(big.NewInt(c.PublicUID.Int64).Text(32)),
+		AuctionConfigurationV2Attrs: m.resourceAttrs(c),
+		App: admin.App{
+			ID:       c.App.ID,
+			AppAttrs: appMapper{}.resourceAttrs(&c.App),
+		},
+		Segment: segment,
+	}
+}
+
+func (m auctionConfigurationV2Mapper) resourceAttrs(c *db.AuctionConfiguration) admin.AuctionConfigurationV2Attrs {
+	var segmentID *int64
+	if c.SegmentID != nil && c.SegmentID.Valid {
+		segmentID = &c.SegmentID.Int64
+	} else {
+		segmentID = nil
+	}
+
+	return admin.AuctionConfigurationV2Attrs{
+		Name:                     c.Name.String,
+		AppID:                    c.AppID,
+		AdType:                   c.AdType.Domain(),
+		Pricefloor:               c.Pricefloor,
+		SegmentID:                segmentID,
+		ExternalWinNotifications: c.ExternalWinNotifications,
+		Demands:                  db.StringArrayToAdapterKeys(&c.Demands),
+		Bidding:                  db.StringArrayToAdapterKeys(&c.Bidding),
+		AdUnitIDs:                c.AdUnitIds,
+		Timeout:                  c.Timeout,
+	}
+}
