aec7c21d228b3fdb9773196a90456490ed54a54f
AS <as@bidon.org>
2024-06-12 13:48:46 +0200

    [BDN-655] Add AuctionConfigurationV2 CRUD (Admin)[Frontend] (#494)
    * [BDN-655] Add AuctionConfigurationV2 CRUD (Admin)[Frontend]

---
+++ web/bidon_ui/pages/v2/auction_configurations/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <CreateResourceButton
+    label="New Auction Configuration"
+    :resources-path="resourcesPath"
+  />
+  <ResourcesTable :columns="columns" :resources-path="resourcesPath" />
+</template>
+
+<script setup>
+import { ResourceTableFields } from "@/constants";
+
+const columns = [
+  ResourceTableFields.PublicUid,
+  ResourceTableFields.App,
+  { field: "name", header: "Name" },
+  { field: "auctionKey", header: "Auction Key", copyable: true },
+  ResourceTableFields.AdType,
+  { field: "pricefloor", header: "Price Floor" },
+  ResourceTableFields.Segment,
+];
+const resourcesPath = "/v2/auction_configurations";
+</script>
