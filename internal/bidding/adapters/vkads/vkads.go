4d248491f2f94967c3a6352241323e3e7e3d7554
AS <as@bidon.org>
2024-07-22 14:09:51 +0200

    [BDN-680] Integrate VK Ads bidding (#512)
    * [BDN-680] Integrate VK Ads bidding

---
+++ internal/bidding/adapters/vkads/vkads.go
@@ -0,0 +1,255 @@
+package vkads
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/bidon-io/bidon-backend/internal/ad"
+	"github.com/bidon-io/bidon-backend/internal/adapter"
+	"github.com/bidon-io/bidon-backend/internal/bidding/adapters"
+	"github.com/bidon-io/bidon-backend/internal/bidding/openrtb"
+	"github.com/bidon-io/bidon-backend/internal/sdkapi/schema"
+	"github.com/gofrs/uuid/v5"
+	"github.com/prebid/openrtb/v19/adcom1"
+	"github.com/prebid/openrtb/v19/openrtb2"
+)
+
+type VKAdsAdapter struct {
+	TagID string
+	AppID string
+}
+
+var bannerFormats = map[ad.Format][2]int64{
+	ad.BannerFormat:      {320, 50},
+	ad.LeaderboardFormat: {728, 90},
+	ad.MRECFormat:        {300, 250},
+	ad.AdaptiveFormat:    {320, 50},
+	ad.EmptyFormat:       {320, 50}, // Default
+}
+
+const (
+	rewardedWidth  int64 = 1920
+	rewardedHeight int64 = 1080
+)
+
+func (a *VKAdsAdapter) banner(br *schema.BiddingRequest) *openrtb2.Imp {
+	size := bannerFormats[br.Imp.Format()]
+
+	if br.Imp.IsAdaptive() && br.Device.IsTablet() {
+		size = bannerFormats[ad.LeaderboardFormat]
+	}
+
+	w, h := size[0], size[1]
+
+	return &openrtb2.Imp{
+		Instl: 0,
+		Banner: &openrtb2.Banner{
+			W:   &w,
+			H:   &h,
+			Pos: adcom1.PositionAboveFold.Ptr(),
+		},
+	}
+}
+
+func (a *VKAdsAdapter) interstitial() *openrtb2.Imp {
+	return &openrtb2.Imp{
+		Instl: 1,
+		Banner: &openrtb2.Banner{
+			Pos: adcom1.PositionFullScreen.Ptr(),
+		},
+	}
+}
+
+func (a *VKAdsAdapter) rewarded(br *schema.BiddingRequest) *openrtb2.Imp {
+	w, h := rewardedWidth, rewardedHeight
+	return &openrtb2.Imp{
+		Banner: &openrtb2.Banner{
+			W: &w,
+			H: &h,
+		},
+		BidFloor:    br.Imp.GetBidFloor(),
+		BidFloorCur: "USD",
+	}
+}
+
+func (a *VKAdsAdapter) CreateRequest(request openrtb.BidRequest, br *schema.BiddingRequest) (openrtb.BidRequest, error) {
+	if a.AppID == "" {
+		return request, errors.New("AppID is empty")
+	}
+	if a.TagID == "" {
+		return request, errors.New("TagID is empty")
+	}
+
+	var imp *openrtb2.Imp
+	switch br.Imp.Type() {
+	case ad.BannerType:
+		imp = a.banner(br)
+	case ad.InterstitialType:
+		imp = a.interstitial()
+	case ad.RewardedType:
+		imp = a.rewarded(br)
+	default:
+		return request, errors.New("unknown impression type")
+	}
+
+	impId, _ := uuid.NewV4()
+	imp.ID = impId.String()
+	imp.TagID = a.TagID
+
+	request.Imp = []openrtb2.Imp{*imp}
+	request.Cur = []string{"USD"}
+
+	request.User = &openrtb.User{
+		ID: br.User.IDG,
+	}
+	request.App.ID = a.AppID
+	request.Ext = json.RawMessage(`{"pid":111}`)
+
+	return request, nil
+}
+
+func (a *VKAdsAdapter) ExecuteRequest(ctx context.Context, client *http.Client, request openrtb.BidRequest) *adapters.DemandResponse {
+	dr := &adapters.DemandResponse{
+		DemandID:  adapter.VKAdsKey,
+		RequestID: request.ID,
+		TagID:     a.TagID,
+	}
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	dr.RawRequest = string(requestBody)
+
+	url := "https://ad.mail.ru/api/" + a.TagID
+	httpReq, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	httpReq.Header.Add("Content-Type", "application/json")
+
+	httpResp, err := client.Do(httpReq)
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+	defer httpResp.Body.Close()
+
+	respBody, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		dr.Error = err
+		return dr
+	}
+
+	dr.RawResponse = string(respBody)
+	dr.Status = httpResp.StatusCode
+
+	return dr
+}
+
+func (a *VKAdsAdapter) ParseBids(dr *adapters.DemandResponse) (*adapters.DemandResponse, error) {
+	switch dr.Status {
+	case http.StatusNoContent:
+		return dr, nil
+	case http.StatusServiceUnavailable:
+		fallthrough
+	case http.StatusBadRequest:
+		fallthrough
+	case http.StatusUnauthorized:
+		fallthrough
+	case http.StatusForbidden:
+		return dr, fmt.Errorf("unauthorized request: " + strconv.Itoa(dr.Status))
+	case http.StatusOK:
+		break
+	default:
+		return dr, fmt.Errorf("unexpected status code: " + strconv.Itoa(dr.Status))
+	}
+
+	var bidResponse openrtb2.BidResponse
+	fixedResponse := fixRawResponse(&dr.RawResponse)
+	err := json.Unmarshal([]byte(fixedResponse), &bidResponse)
+	if err != nil {
+		return dr, err
+	}
+
+	seat := bidResponse.SeatBid[0]
+	bid := seat.Bid[0]
+
+	dr.Bid = &adapters.BidDemandResponse{
+		ID:       bid.ID,
+		ImpID:    bid.ImpID,
+		Price:    bid.Price,
+		Payload:  bid.AdM,
+		DemandID: adapter.VKAdsKey,
+		AdID:     bid.AdID,
+		SeatID:   seat.Seat,
+		LURL:     bid.LURL,
+		NURL:     bid.NURL,
+		BURL:     bid.BURL,
+	}
+
+	return dr, nil
+}
+
+// TODO: Find a better way to fix the raw response.
+// VK Ads return bid response with boolean value for "bundle" field instead of string. This function fixes it.
+func fixRawResponse(rawResponse *string) string {
+	data := map[string]any{}
+	err := json.Unmarshal([]byte(*rawResponse), &data)
+	if err != nil {
+		return *rawResponse
+	}
+
+	if seatbids, ok := data["seatbid"].([]any); ok && len(seatbids) > 0 {
+		if sb, ok := seatbids[0].(map[string]any); ok {
+			if bids, ok := sb["bid"].([]any); ok && len(bids) > 0 {
+				if b, ok := bids[0].(map[string]any); ok {
+					if bundle, ok := b["bundle"].(bool); ok {
+						b["bundle"] = strconv.FormatBool(bundle)
+					}
+				}
+			}
+		}
+	}
+
+	updatedData, err := json.Marshal(data)
+	if err != nil {
+		return *rawResponse
+	}
+
+	return string(updatedData)
+}
+
+// Builder builds a new instance of the VKAds adapter for the given bidder with the given config.
+func Builder(cfg adapter.ProcessedConfigsMap, client *http.Client) (*adapters.Bidder, error) {
+	vkCfg := cfg[adapter.VKAdsKey]
+
+	appID, ok := vkCfg["app_id"].(string)
+	if !ok || appID == "" {
+		appID = ""
+	}
+
+	tagID, ok := vkCfg["tag_id"].(string)
+	if !ok {
+		tagID = ""
+	}
+
+	adpt := &VKAdsAdapter{
+		AppID: appID,
+		TagID: tagID,
+	}
+
+	bidder := &adapters.Bidder{
+		Adapter: adpt,
+		Client:  client,
+	}
+
+	return bidder, nil
+}
