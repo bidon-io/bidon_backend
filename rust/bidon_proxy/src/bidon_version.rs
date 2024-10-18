4cf6d3f098f34def73126ecac68ff6f9b18926e0
BF <bf@bidon.org>
2024-10-18 14:44:59 +0200

    WIP first revision of rust bidon_proxy

---
+++ rust/bidon_proxy/src/bidon_version.rs
@@ -0,0 +1,37 @@
+use std::fmt;
+
+// TODO use the following constants in your code
+
+/// Header - `x-bidon-version` - version of the bidon server.
+pub const X_BIDON_VERSION_HEADER: &str = "x-bidon-version";
+
+pub const BIDON_VERSION: &str = "0.0.1"; // TODO: Update version
+
+/// Wrapper for a string being used as an X-Span-ID.
+#[derive(Debug, Clone)]
+pub struct XBidonVersionString(pub String);
+
+impl XBidonVersionString {
+    /// Extract an X-Bidon-version from a request header if present, and if not
+    /// generate a new one.
+    pub fn get_or_generate<T>(req: &hyper::Request<T>) -> Self {
+        let x_bidon_version = req.headers().get(X_BIDON_VERSION_HEADER);
+
+        x_bidon_version
+            .and_then(|x| x.to_str().ok())
+            .map(|x| XBidonVersionString(x.to_string()))
+            .unwrap_or_default()
+    }
+}
+
+impl Default for XBidonVersionString {
+    fn default() -> Self {
+        XBidonVersionString(BIDON_VERSION.to_string())
+    }
+}
+
+impl fmt::Display for XBidonVersionString {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
---
+++ rust/bidon_proxy/src/lib.rs
@@ -0,0 +1,55 @@
+#![allow(missing_docs, trivial_casts, unused_variables, unused_mut, unused_imports, unused_extern_crates, unused_attributes, non_camel_case_types)]
+#![allow(clippy::derive_partial_eq_without_eq, clippy::disallowed_names)]
+
+use async_trait::async_trait;
+use futures::Stream;
+use std::error::Error;
+use std::task::{Context, Poll};
+use swagger::{ApiError, ContextWrapper};
+use serde::{Deserialize, Serialize};
+type ServiceError = Box<dyn Error + Send + Sync + 'static>;
+
+pub const BASE_PATH: &str = "";
+pub const API_VERSION: &str = "1.0.0";
+
+mod auth;
+pub use auth::{AuthenticationApi, Claims};
+pub use api::{
+    Api,
+    GetAuctionResponse,
+    GetConfigResponse,
+    GetOpenApiSpecResponse,
+    PostClickResponse,
+    PostLossResponse,
+    PostRewardResponse,
+    PostShowResponse,
+    PostStatsResponse,
+    PostWinResponse,
+};
+
+
+
+#[cfg(feature = "client")]
+pub mod client;
+
+// Re-export Client as a top-level name
+#[cfg(feature = "client")]
+pub use client::Client;
+
+#[cfg(feature = "server")]
+pub mod server;
+
+// Re-export router() as a top-level name
+#[cfg(feature = "server")]
+pub use self::server::Service;
+
+#[cfg(feature = "server")]
+pub mod context;
+
+pub mod models;
+
+#[cfg(any(feature = "client", feature = "server"))]
+pub(crate) mod header;
+mod impls;
+pub mod bidon_version;
+mod api;
