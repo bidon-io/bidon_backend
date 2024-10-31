9d412dec066049387ff306c574a524f828e2e262
AS <as@bidon.org>
2024-10-31 09:45:32 +0100

    [BDN-XXX] Upgrade nuxt and node (#578)
    * [BDN-XXX] Upgrade nuxt and node

--- web/bidon_ui/nuxt.config.ts
+++ web/bidon_ui/nuxt.config.ts
@@ -3,24 +3,32 @@ export default defineNuxtConfig({
   alias: {
     assets: "/<rootDir>/assets",
   },
+
   ssr: false,
+
   css: [
     "primevue/resources/themes/lara-light-blue/theme.css",
     "primevue/resources/primevue.css",
     "primeicons/primeicons.css",
   ],
+
   components: [
     {
       path: "~/components",
       pathPrefix: false,
     },
   ],
+
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vee-validate/nuxt"],
+
   build: {
     transpile: ["primevue"],
   },
+
   routeRules: {
     "/auth/**": { proxy: "http://localhost:1323/auth/**" },
     "/api/**": { proxy: "http://localhost:1323/api/**" },
   },
+
+  compatibilityDate: "2024-10-31",
 });
