ef013a7e2bb030df5ccae349c85d9514ef210f30
BF <bf@bidon.org>
2024-11-21 15:55:51 +0100

    Renamed auction to bidding

--- proxy/src/bidding/proxy.rs
+++ proxy/src/bidding/proxy.rs
@@ -1,30 +1,30 @@
-use crate::auction::Api;
-use crate::auction::AuctionError;
+use crate::bidding::Api;
+use crate::bidding::BiddingError;
 use crate::com::iabtechlab::openrtb::v3::Openrtb;
 use crate::galaxy::v1::bidding_service_client::BiddingServiceClient;
 use tonic::transport::Channel;
 use tonic::Request;
 
-pub struct SimpleAuction {
+pub struct ProxyBiddingService {
     grpc_client: BiddingServiceClient<Channel>,
 }
-//todo rename to proxy/ grpc bidding service
-impl SimpleAuction {
+
+impl ProxyBiddingService {
     pub async fn new(grpc_url: String) -> Result<Self, Box<dyn std::error::Error>> {
         let grpc_client = BiddingServiceClient::connect(grpc_url).await?;
-        Ok(SimpleAuction { grpc_client })
+        Ok(ProxyBiddingService { grpc_client })
     }
 }
 
 #[async_trait::async_trait]
-impl Api for SimpleAuction {
-    async fn bid(&mut self, auction_request: Openrtb) -> Result<Openrtb, AuctionError> {
-        let request = Request::new(auction_request);
+impl Api for ProxyBiddingService {
+    async fn bid(&mut self, bidding_request: Openrtb) -> Result<Openrtb, BiddingError> {
+        let request = Request::new(bidding_request);
         let response = self
             .grpc_client
             .bid(request)
             .await
-            .map_err(|e| AuctionError::new(e.to_string()))?;
+            .map_err(|e| BiddingError::new(e.to_string()))?;
         Ok(response.into_inner())
     }
 }
