4a3cd99353c89874e3d7e1207c686fd78faea81d
AJ <aj@bidon.org>
2024-11-27 14:24:26 +0100

    [BDN-XXX] Ad Cache ad_unit_uid rename (#602)

--- internal/sdkapi/schema/ad_cache_object.go
+++ internal/sdkapi/schema/ad_cache_object.go
@@ -4,5 +4,5 @@ type AdCacheObject struct {
 	DemandID  string  `json:"demand_id" validate:"required"`
 	Timestamp int64   `json:"timestamp" validate:"required"`
 	Price     float64 `json:"price" validate:"required"`
-	AdUnitID  *string `json:"ad_unit_id,omitempty"`
+	AdUnitUID *string `json:"ad_unit_uid,omitempty"`
 }
