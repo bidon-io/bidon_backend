c639aff1ed6b4acd18b7f6058feff2f2feaf44fd
AS <as@bidon.org>
2025-03-26 18:28:28 +0300

    [BDN-917] Add seed migrations (#709)
    * [BDN-917] Add seed migrations
    * [BDN-917] Add docker compose seed service

---
+++ cmd/bidon-seed/seeds/20250325114723_demand_sources.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+INSERT INTO demand_sources (api_key, human_name, created_at, updated_at)
+VALUES
+('bidmachine', 'BidMachine', NOW(), NOW()),
+('admob', 'AdMob', NOW(), NOW()),
+('applovin', 'AppLovin', NOW(), NOW()),
+('unityads', 'Unity Ads', NOW(), NOW()),
+('meta', 'Meta', NOW(), NOW()),
+('dtexchange', 'DT Exchange', NOW(), NOW()),
+('amazon', 'Amazon', NOW(), NOW()),
+('bigoads', 'Bigo Ads', NOW(), NOW()),
+('chartboost', 'Chartboost', NOW(), NOW()),
+('gam', 'Google Ad Manager', NOW(), NOW()),
+('inmobi', 'InMobi', NOW(), NOW()),
+('ironsource', 'ironSource', NOW(), NOW()),
+('mintegral', 'Mintegral', NOW(), NOW()),
+('mobilefuse', 'MobileFuse', NOW(), NOW()),
+('vkads', 'VK Ads', NOW(), NOW()),
+('vungle', 'Vungle', NOW(), NOW()),
+('yandex', 'Yandex', NOW(), NOW())
+ON CONFLICT (api_key) DO NOTHING;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+-- +goose StatementEnd
