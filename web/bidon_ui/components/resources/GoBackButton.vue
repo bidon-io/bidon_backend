b9e497d12b151b55349f502a87934b990c6aff53
AS <as@bidon.org>
2025-05-06 11:11:49 +0300

    [BDN-XXX] Fix BackButton [UI] (#741)

--- web/bidon_ui/components/resources/GoBackButton.vue
+++ web/bidon_ui/components/resources/GoBackButton.vue
@@ -9,28 +9,9 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  path: {
-    type: String,
-    default: "/",
-  },
-});
-
 const router = useRouter();
-const route = useRoute();
-const nuxtApp = useNuxtApp();
 
 const goBack = () => {
-  const previousRoute = nuxtApp.$routeHistory().getPrevious();
-
-  if (
-    previousRoute &&
-    previousRoute.path !== "" &&
-    previousRoute.path !== route.path
-  ) {
-    router.push(previousRoute);
-  } else {
-    router.push(props.path);
-  }
+  router.back();
 };
 </script>
