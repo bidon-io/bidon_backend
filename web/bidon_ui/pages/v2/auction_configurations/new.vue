436321f97b0117bcb60477598aeffe444b77eb7c
AS <as@bidon.org>
2025-06-10 18:03:17 +0200

    [BDN-XXX] Remove Outdated Clone BTN (#768)

--- web/bidon_ui/pages/v2/auction_configurations/new.vue
+++ web/bidon_ui/pages/v2/auction_configurations/new.vue
@@ -3,43 +3,13 @@
     <NavigationContainer>
       <GoBackButton :path="resourcesPath" />
     </NavigationContainer>
-    <AuctionConfigurationsForm
-      v-if="isReady"
-      :value="resource"
-      @submit="handleSubmit"
-    />
+    <AuctionConfigurationsForm :value="resource" @submit="handleSubmit" />
   </PageContainer>
 </template>
 
 <script setup>
-import { useAsyncState } from "@vueuse/core";
-import axios from "@/services/ApiService";
-
-const route = useRoute();
+const resource = {};
 const resourcesPath = "/v2/auction_configurations";
-const cloneId = route.query.clone;
-
-const { state: resource, isReady } = useAsyncState(async () => {
-  if (cloneId) {
-    try {
-      const response = await axios.get(`${resourcesPath}/${cloneId}`);
-      const sourceResource = response.data;
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { id, publicUid, auctionKey, ...clonedResource } = sourceResource;
-      return {
-        ...clonedResource,
-        name: `${sourceResource.name} (Copy)`,
-        isDefault: false, // Don't clone as default
-      };
-    } catch (error) {
-      console.error("Failed to fetch source auction configuration:", error);
-      return {};
-    }
-  }
-  return {};
-}, {});
-
 const handleSubmit = useCreateResource({
   path: resourcesPath,
   message: "Auction configuration created!",
