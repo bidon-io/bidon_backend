173360e39231b75677bf674b3de2fa1df6dcaa75
AJ <aj@bidon.org>
2025-08-08 13:48:55 +0200

    [APD-XXX] Return bm only for mb (#791)

--- internal/sdkapi/v2/apihandlers/config_handler.go
+++ internal/sdkapi/v2/apihandlers/config_handler.go
@@ -92,7 +92,7 @@ func (h *ConfigHandler) Handle(c echo.Context) error {
 	if req.raw.Regulations != nil {
 		isCOPPA = req.raw.Regulations.COPPA
 	}
-	chardonnayHack := req.app.ID == 735400 || req.app.ID == 735401 || req.app.ID == 735402 || req.app.ID == 735370
+	chardonnayHack := req.app.ID == 735400 || req.app.ID == 735401 || req.app.ID == 735402 || req.app.ID == 735370 || req.app.ID == 735354
 	adapters := make(map[adapter.Key]sdkapi.AdapterInitConfig, len(adapterInitConfigs))
 
 	var bidMachinePlacements map[string]string
